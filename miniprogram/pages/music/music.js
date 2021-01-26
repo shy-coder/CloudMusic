587444243f5529c40aeb2dd40245d38c4e3cc678
shy-coder <2437309168@qq.com>
2021-01-26 21:13:44 +0800

    day02
    初识自定义组件和使用微信小程序提供的组件

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -1,13 +1,62 @@
 // pages/music/music.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    imgUrls: [{
+      url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/SLfispSeeEnb6Ezs0cNjBw==/109951165666128356.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+    }],
+    playlist: [{
+      "id":"1001",
+      "playCount":221940,
+      "name":"你一定要在自己热爱的世界里闪闪放光啊",
+      "picUrl":"http://p2.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y140"
+    },
+    {
+      "id":"1002",
+      "playCount":601940.3,
+      "name":"青春与理想·摇滚精选集",
+      "picUrl":"http://p2.music.126.net/uEOIYkdUKS0Y-bGvEAI_4w==/109951165064841352.jpg?param=140y140"
+    },
+    {
+      "id":"1003",
+      "playCount":1082,
+      "name":"#幸福#有幸被爱，余生是你",
+      "picUrl":"http://p1.music.126.net/0u7dhriB29wrDp_GyCrLFw==/109951165664303246.jpg?param=140y140"
+    },
+    {
+      "id":"1004",
+      "playCount":270000,
+      "name":"国摇|热爱·是所有理由和答案",
+      "picUrl":"http://p2.music.126.net/uMkW4MvbmMsWnaxPonDehw==/109951165268204866.jpg?param=140y140"
+    },
+    {
+      "id":"1005",
+      "playCount":393,
+      "name":"喜欢就是要直接见面",
+      "picUrl":"http://p1.music.126.net/_rFjQsgRJfTQT55Wuv7T2Q==/109951165660082678.jpg?param=140y140"
+    },
+    {
+      "id":"1006",
+      "playCount":400000,
+      "name":"刺猬乐队-燥起来",
+      "picUrl":"http://p2.music.126.net/vtcXjGbnqabQ0GPuUHL91A==/109951164266476629.jpg?param=140y140"
+    }]
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
