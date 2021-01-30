a4bd0e36e80e50e14795db27b1a317c87100f8ca
shy-coder <2437309168@qq.com>
2021-01-30 23:02:23 +0800

    day06
    播放页面布局

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -36,6 +36,7 @@ exports.main = async (event, context) => {
     ctx.body = res.data
   })
 
+  //根据歌曲id获取歌曲播放的url
   app.router('musicUrl', async (ctx, next) => {
     const res = await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
     ctx.body = res.data
